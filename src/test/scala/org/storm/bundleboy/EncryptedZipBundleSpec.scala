2231623d1cd824c8843bf4c6080348069d33561d
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2014-01-07 18:55:17 +0100

    Refactored project structure.

--- src/test/scala/org/storm/bundleboy/EncryptedZipBundleSpec.scala
+++ src/test/scala/org/storm/bundleboy/EncryptedZipBundleSpec.scala
@@ -11,16 +11,16 @@ class EncryptedZipBundleSpec extends FlatSpec with ShouldMatchers {
   
   val testTxtContents = "This is a test file.\n"
 
-  "EncryptedClassLoader" should "load the class correctly" in {
-    val loader = new Bundle.EncryptedZipFileClassLoader("resources/example-classes.bundle", "password")
+  "EncryptedZipBundle.ClazzLoader" should "load the class correctly" in {
+    val loader = new EncryptedZipBundle.ClazzLoader("resources/example-classes.bundle", "password")
     val cls = loader.findClass("org.storm.bundleboy.TestExampleClass")
     val inst = cls.newInstance
     val string = cls.getMethod("someString").invoke(inst)
     string should equal ("test string")
   }
 
-  "Bundle.EncryptedZipFile" should "fetch the resource correctly" in {
-    val bundle = new Bundle.EncryptedZipFile("testBundle", "resources/example-classes.bundle", "password")
+  "EncryptedZipBundle" should "fetch the resource correctly" in {
+    val bundle = new EncryptedZipBundle("testBundle", "resources/example-classes.bundle", "password")
     import bundle.charReader
     import Encodings.`UTF-8`
     val contents = (bundle / "docs" / "test.txt").slurp[Char]
@@ -28,7 +28,7 @@ class EncryptedZipBundleSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "fetch the resources using Dynamic" in {
-    val bundle = new Bundle.EncryptedZipFile("testBundle", "resources/example-classes.bundle", "password")
+    val bundle = new EncryptedZipBundle("testBundle", "resources/example-classes.bundle", "password")
     val contents = bundle.files.docs.file("test.txt")
     import bundle.charReader
     import Encodings.`UTF-8`
@@ -42,7 +42,7 @@ class EncryptedZipBundleSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "instantiate the class object" in {
-    val bundle = new Bundle.EncryptedZipFile("testBundle", "resources/example-classes.bundle", "password")
+    val bundle = new EncryptedZipBundle("testBundle", "resources/example-classes.bundle", "password")
     val cls = bundle.classes.org.storm.bundleboy.TestExampleClass.get
     val inst = cls.newInstance
     val string = cls.getMethod("someString").invoke(inst)
@@ -50,7 +50,7 @@ class EncryptedZipBundleSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "retrieve all the classes in a package" in {
-    val bundle = new Bundle.EncryptedZipFile("testBundle", "resources/example-classes.bundle", "password")
+    val bundle = new EncryptedZipBundle("testBundle", "resources/example-classes.bundle", "password")
     val cs = bundle.classes.org.storm.bundleboy.all
     cs.map(_.getSimpleName) should equal (Set("TestExampleClass", "TestExampleClass2"))
     val cs2 = bundle.classes.org.storm.all
@@ -58,7 +58,7 @@ class EncryptedZipBundleSpec extends FlatSpec with ShouldMatchers {
   }
 
   it should "retrieve all the subclasses in a package" in {
-    val bundle = new Bundle.EncryptedZipFile("testBundle", "resources/example-classes.bundle", "password")
+    val bundle = new EncryptedZipBundle("testBundle", "resources/example-classes.bundle", "password")
     val cs = bundle.classes.org.storm.bundleboy.subclasses(classOf[Serializable])
     cs.map(_.getSimpleName) should equal (Set("TestExampleClass"))
   }
