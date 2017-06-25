f3ab97387cbf37520ea2b48238dbe54a8502717b
Bertrand Kang <bertrand.kang@gmail.com>
2017-06-25 21:30:02 +0200

    Wrap lines.

--- src/main/scala/org/stormenroute/bundleboy/ClassPathBundle.scala
+++ src/main/scala/org/stormenroute/bundleboy/ClassPathBundle.scala
@@ -13,13 +13,14 @@ import scala.collection.convert.decorateAsScala._
 
 class ClassPathBundle(classloader: ClassLoader)
 extends Bundle {
-  def name = s"ClassPathBundle($classloader)" 
+  def name = s"ClassPathBundle($classloader)"
 
   def loadPaths(path: String) = throw new UnsupportedOperationException
 
   def loadClass(name: String) = classloader.loadClass(name)
 
-  def loadSubclasses(packageName: String, baseClass: Class[_]) = throw new UnsupportedOperationException
+  def loadSubclasses(packageName: String, baseClass: Class[_]) =
+    throw new UnsupportedOperationException
 
   def loadStream(path: String) = classloader.getResourceAsStream(path)
 }
